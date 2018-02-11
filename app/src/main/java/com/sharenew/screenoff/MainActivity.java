7ac6854f5db199e3ab4b2413de9caaca2b6a1f07
zxq <1723544852@qq.com>
2018-02-11 13:53:02 +0800

    修改一些东西。使得第一次能主动申请设备管理权限。参考地址:http://blog.csdn.net/world_java/article/details/20623633

--- app/src/main/java/com/sharenew/screenoff/MainActivity.java
+++ app/src/main/java/com/sharenew/screenoff/MainActivity.java
@@ -1,28 +1,18 @@
 package com.sharenew.screenoff;
 
-import android.Manifest;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import static android.content.Context.POWER_SERVICE;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonOff;
@@ -42,16 +32,19 @@ public class MainActivity extends AppCompatActivity {
         }
     };
     DevicePolicyManager policyManager;
-
+    ComponentName adminReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonOff = (Button) findViewById(R.id.button_off);
+        adminReceiver = new ComponentName(MainActivity.this, ScreenOffAdminReceiver.class);
         buttonOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ComponentName adminReceiver = new ComponentName(MainActivity.this, ScreenOffAdminReceiver.class);
+
+
+
                 boolean admin = policyManager.isAdminActive(adminReceiver);
                 if (admin) {
                     policyManager.lockNow();
@@ -71,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
         });
         mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
         policyManager = (DevicePolicyManager) MainActivity.this.getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,  adminReceiver);
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"开启后就可以使用锁屏功能了...");//显示位置见图二
+
+        startActivityForResult(intent, 0);
     }
 
 
@@ -84,5 +83,33 @@ public class MainActivity extends AppCompatActivity {
         mWakeLock.acquire();
         mWakeLock.release();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        isOpen();
+    }
+
+    /**
+     * 检测用户是否开启了超级管理员
+     */
+    private void isOpen() {
+        if(policyManager.isAdminActive(adminReceiver)){//判断超级管理员是否激活
+
+            Toast.makeText(MainActivity.this,"设备已被激活",
+                    Toast.LENGTH_LONG).show();
+
+        }else{
+
+            Toast.makeText(MainActivity.this,"设备没有被激活",
+                    Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+
+
+
 
 }
